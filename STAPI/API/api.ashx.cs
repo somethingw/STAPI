43239779aad1cf4408162a09b7e6c46e28b3f71c
somethingw <1099236817@qq.com>
2019-02-27 16:16:28 +0800

    完成数据的验签以及包体的获取

--- STAPI/API/api.ashx.cs
+++ STAPI/API/api.ashx.cs
@@ -1,6 +1,7 @@
 ﻿using aliyun_api_gateway_sdk_ext.Sign;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -11,13 +12,28 @@ namespace STAPI.API
     /// </summary>
     public class api : IHttpHandler
     {
+        //返回状态码对应说明
+        private Dictionary<int, string> code = new Dictionary<int, string>
+        {
+            {200,"success"},{400,"request error"},{401,"request auth error"},{403,"request forbidden"},
+            {404,"service not found"},{429,"too many requests"},{460,"request parameter error"},{500,"service error"},{503,"service not available"}
+        };
+
 
         public void ProcessRequest(HttpContext context)
         {
-            if (isSign.Sign(context)) {
-                context.Response.ContentType = "text/plain";
-                context.Response.Write("Hello World");
+            
+            context.Response.ContentType = "text/plain";
+            Stream stream = context.Request.InputStream;
+            string json = string.Empty;//得到json包
+            string responseJson = string.Empty;
+            if (stream.Length != 0)
+            {
+                StreamReader streamReader = new StreamReader(stream);
+                json = streamReader.ReadToEnd();
             }
+            context.Response.Write(isSign.Sign(context));
+            
             
         }
 
